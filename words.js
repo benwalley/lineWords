b00acad8fe031afde7d16ccda5d7e6713e41b2cb
benwalley <benwalleyorigami@gmail.com>
2017-12-09 23:43:33 -0600

    added controll settings

--- words.js
+++ words.js
@@ -5,19 +5,70 @@ var backgroundCanvas = document.getElementById("backgroundCanvas")
 var backCtx = backgroundCanvas.getContext("2d")
 
 var data = {
-	number: 2000,
-	speed: 4,
+	number: 200,
+	speed: 3,
 	size: 1, 
 	background: "grey", 
-	word: "BEN", 
+	word: "TEST", 
 	margin: 50,
-	color: "black"
+	color: "black",
+	lineColor: "black",
+	lineThickness: .1
 }
 
 var circles = [];
 var colors = {};
 var letterSize = undefined
-var longestLine = 15;
+var longestLine = 50;
+var resetButton = $("#resetButton");
+var startButton = $("#startButton");
+var wordInput = $("#wordInput");
+var numberInput = $("#numberInput");
+var showDotsInput = $("#showDotsInput");
+var showLinesInput = $("#showLinesInput");
+var backgroundColorInput = $("#backgroundColorInput");
+var ballColorInput = $("#ballColorInput");
+var lineColorInput = $("#lineColorInput");
+var ballSizeInput = $("#ballSizeInput");
+var lineThicknessInput = $("#lineThicknessInput");
+var ballSpeedInput = $("#ballSpeedInput");
+var maxLineInput = $("#maxLineInput");
+
+var showDots = true;
+var showLines = true;
+
+//whenever any input is changed, update data
+
+var inp = $("input");
+
+inp.change(updateData)
+
+function updateData(){
+	data.word = wordInput.val();
+	data.number = numberInput.val();
+	// controll whether dots are shown
+	if(showDotsInput.is(":checked")){
+		showDots = true
+	}else if(!showDotsInput.is(":checked")){
+		showDots = false
+	}
+	//controll whether lines are shown
+	if(showLinesInput.is(":checked")){
+		showLines = true
+	}else if(!showLinesInput.is(":checked")){
+		showLines = false
+	}
+	canvas.style.background = backgroundColorInput.val();
+	data.color = ballColorInput.val();
+	data.lineColor = lineColorInput.val();
+	data.size = ballSizeInput.val()/100;
+	data.lineThickness = lineThicknessInput.val()/100;
+	data.speed = ballSpeedInput.val()/10
+	longestLine = maxLineInput.val()/10
+
+
+
+}
 
 
 canvas.width = window.innerWidth
@@ -26,6 +77,9 @@ canvas.height = window.innerHeight
 backgroundCanvas.width = window.innerWidth
 backgroundCanvas.height = window.innerHeight
 
+
+// functions to create words, letters, circles and lines//
+
 //create a single letter
 function drawLetter(letter, size, color, x, y, context = backCtx, font = 'Helvetica'){
 	context.font = "bold " + size + " Helvetica";
@@ -33,10 +87,6 @@ function drawLetter(letter, size, color, x, y, context = backCtx, font = 'Helvet
 	context.fillText(letter, x, y)
 }
 
-// array of each dot on each color
-
-//create a word, using drawLetter function
-//each letter has different color
 function drawWord(word){
 	arrWord = Array.from(word);
 	for(var i = 0; i < arrWord.length; i++){
@@ -46,9 +96,11 @@ function drawWord(word){
 		
 		// size for current letter
 		letterSize = (canvas.width- data.margin*2)/arrWord.length
-		drawLetter(arrWord[i], letterSize + "px", color, data.margin+(i*letterSize),canvas.height/4 + letterSize, backCtx)
+		drawLetter(arrWord[i], letterSize + "px", color, data.margin+(i*letterSize),letterSize + canvas.height/5, backCtx)
 	}
 }
+
+
 
 function drawCircle(x, y, r, fill = "black"){
 	ctx.beginPath();
@@ -63,20 +115,32 @@ function drawLine(x,y,endX, endY, width = 1){
 	ctx.moveTo(x,y);
 	ctx.lineTo(endX, endY);
 	ctx.lineWidth = width
-	ctx.strokeStyle = "white"
+	ctx.strokeStyle = data.lineColor
 	ctx.stroke();
+}
+
+function start(){
+	cancelAnimationFrame(myAnimation)
+	circles = []
+	backCtx.clearRect(0,0,canvas.width,canvas.height);
+	drawWord(data.word)
+	updateData()
+	createCircles(data);
+	myAnimation = requestAnimationFrame(draw)
 }
 
 // call the draw word function with word from data object
 drawWord(data.word)
 
+// check if any two circles are on the same letter. if they are, draw line between them
+// currently it will draw it both ways. it could be improved
 function checkOnLetter(){
 	for (var i = 0; i < circles.length; i++) {
 		for (var q = 0; q < circles.length; q++) {
 			if(circles[i].backgroundColor == circles[q].backgroundColor && circles[i].backgroundColor != 0){
 				var length =(Math.abs(circles[i].x - circles[q].x) + Math.abs(circles[i].y - circles[q].y))/2
 				if(length <= longestLine){
-					drawLine(circles[i].x, circles[i].y, circles[q].x, circles[q].y, .02)
+					drawLine(circles[i].x, circles[i].y, circles[q].x, circles[q].y, data.lineThickness)
 				}
 				
 				
@@ -85,7 +149,7 @@ function checkOnLetter(){
 	}
 }
 
-
+// fill the array with data
 function createCircles(data){
 	for (var i = 0; i < data.number; i++) {
 		circles.push({
@@ -102,6 +166,7 @@ function createCircles(data){
 
 createCircles(data)
 
+// check if any balls hit walls. if they do, reverse velocity
 function checkCollision(x,y){
 	for (var i = 0; i < circles.length; i++) {
 		if(circles[i].x <= 0 || circles[i].x >= canvas.width){
@@ -113,6 +178,7 @@ function checkCollision(x,y){
 	}
 }
 
+//move all circles, based on dx and dy
 function moveCircles(){
 	for (var i = 0; i < circles.length; i++) {
 		circles[i].x += circles[i].dx
@@ -120,6 +186,7 @@ function moveCircles(){
 	}
 }
 
+// check what color the ball is over, on the background image. then put the red value in the backgroundColor data
 function checkColor(){
 	for (var i = 0; i < circles.length; i++) {
 		// get color of red (0 - 255) from the point of circles, and fill their data with it
@@ -128,26 +195,32 @@ function checkColor(){
 }
 
 
-
+// draw all the circles, based on x and y, and r
 function drawCircles(){
 	for (var i = 0; i < circles.length; i++) {
 		drawCircle(circles[i].x, circles[i].y, circles[i].r, circles[i].color)
 	}
 }
 
-
+// draw everything, and request animation frame
 function draw(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	checkCollision()
 	checkColor()
 	moveCircles()
-	// drawCircles()
-	checkOnLetter()
 
-	window.requestAnimationFrame(draw)
+	if(showDots){
+		drawCircles()
+	}
+	if(showLines){
+		checkOnLetter()
+	}
+	
+
+	myAnimation = requestAnimationFrame(draw)
 }
 
-draw()
+myAnimation = requestAnimationFrame(draw)
 
 
 
